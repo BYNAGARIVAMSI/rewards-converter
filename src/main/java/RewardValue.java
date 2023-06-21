9a3bba07e0e3f60e149be6c627a85a9345e7303e
VAMSI <bynagarivamsi.bvs8264@gmail.com>
2023-06-21 15:23:32 +0530

    Add RewardValue class implementation

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,13 @@
+public class RewardValue {
+    public RewardValue(double cashValue) {
+    }
+
+    public String getMilesValue() {
+        return null;
+    }
+
+    public double getCashValue() {
+
+        return 0;
+    }
+}
